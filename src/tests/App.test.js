3be29238ecffa07812c9eac7134a5d7b188c2c64
Maibritt Rittermann <mri@skivecollege.dk>
2022-04-07 12:28:38 +0200

    Mock test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,15 @@ import {render, screen} from '@testing-library/react';
 import App from '../App';
 
 test('renders react component', () => {
-  render(<App />);
-  const divElement = screen.getByText(/Local Food/i);
-  expect(divElement).toBeInTheDocument();
+
+    const mockedUsedNavigate = jest.fn();
+    jest.mock('react-router-dom', () => ({
+        ...jest.requireActual('react-router-dom'),
+        useNavigate: () => mockedUsedNavigate,
+    }));
+  
+    render(<App />);
+
+    const divElement = screen.getByText(/Local Food/i);
+    expect(divElement).toBeInTheDocument();
 });
