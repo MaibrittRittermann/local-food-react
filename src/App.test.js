af1f904a372b8041dc9a299cb2f21e998c7ffd42
Maibritt Rittermann <mri@skivecollege.dk>
2022-04-07 11:09:08 +0200

    No test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Local Food/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
