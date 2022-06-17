ef3e748073568f37e6f29774526567a10abdc6f1
Maibritt Rittermann <mri@skivecollege.dk>
2022-06-17 10:30:28 +0200

    correct

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -18,9 +18,10 @@ export function setJwt(jwt) {
     axios.defaults.headers.common['x-auth-token'] = jwt;
 }
 
-export default {
-    get: axios.get,
+const methods = {
+    get : axios.get,
     post: axios.post,
     put: axios.put,
     delete: axios.delete
 }
+export default methods;
